eb03b3c330c52c21cd4ea746483b558cfebb74d5
Ray Lei <rlei@aligntech.com>
2020-02-19 10:54:26 +0800

    remove action param in validator since it's not necessary

--- lib/new_google_recaptcha/validator.rb
+++ lib/new_google_recaptcha/validator.rb
@@ -3,19 +3,17 @@ require 'net/http'
 module NewGoogleRecaptcha
   class Validator
     attr_reader :score
-    attr_reader :token, :action, :minimum_score
+    attr_reader :token, :minimum_score
 
-    def initialize(token:, action:, minimum_score:)
+    def initialize(token:, minimum_score:)
       @token         = token
-      @action        = action
       @minimum_score = minimum_score
     end
 
     def call
-      uri    = NewGoogleRecaptcha.compose_uri(token)
-      
+      uri = NewGoogleRecaptcha.compose_uri(token)
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if uri.scheme == 'https' 
+      http.use_ssl = true if uri.scheme == 'https'
       result = JSON.parse(http.request(Net::HTTP::Get.new(uri)).body)
 
       @score = result['score'].to_f
@@ -23,7 +21,6 @@ module NewGoogleRecaptcha
       conditions = []
       conditions << !!result['success']
       conditions << (result['score'].to_f >= minimum_score)
-      conditions << (result['action'].to_s == action.to_s)
       conditions.none?(&:!)
     end
   end
